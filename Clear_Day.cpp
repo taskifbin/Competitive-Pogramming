20f81e177a1a8a931b6896864eb1411169c28b63
taskifbin <taskifbin@yahoo.com>
2024-07-09 07:59:14 +0600

    CodeChef

---
+++ Clear_Day.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cin>>a>>b;
+
+    cout<<7-(a+b)<<endl;
+
+    return 0;
+}
+//https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/CLEARDAY
---
+++ Cricket_World_Cup_Qualifier.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    if(n>=12) cout<<"Yes\n";
+    else cout<<"No\n";
+
+    return 0;
+}
+//https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/CWC23QUALIF
